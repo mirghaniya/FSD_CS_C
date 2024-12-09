9dad117e68bf37dbf7a5dce8316bf5b889c3a573
mirghaniya <princerathorr.2002@gmail.com>
2024-12-09 22:29:02 +0530

    initial init

---
+++ Day-12/react-hooks-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CounterApp from "./Components/CounterApp";
+import ImageApp from "./Components/ImageApp";
+
+const App = () => {
+  return (
+    <div>
+      {" "}
+      <ImageApp />
+      {/* <CounterApp />{" "} */}
+    </div>
+  );
+};
+
+export default App;
